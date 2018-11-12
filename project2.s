a740c44a7d447c515bfd95003c4686c1c0bf17c8
Oluwatoni Oshikanlu <39501338+tonioshikanlu@users.noreply.github.com>
2018-11-12 12:27:53 -0500

    Removal of prompt

--- project2.s
+++ project2.s
@@ -1,7 +1,1 @@
-.data
-	string1: .asciiz "Enter four characters"
-.text 				# Assembly instructions
-main:				# Start of main code
-	la $a0, string1		# Load String asking for characters
-	li $v0, 4
-	syscall 		# prints output
+
