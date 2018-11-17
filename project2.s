3ccd6252353cca78b80e0b645a4ee70a13571ec3
Oluwatoni Oshikanlu <39501338+tonioshikanlu@users.noreply.github.com>
2018-11-16 20:04:39 -0500

    This removes the space before the string. It saves the space character to the t8 register and then stores characters in the t7 register if they're not spaces. Afterwards, it jumps to the delete spaces conditional section of my code

--- project2.s
+++ project2.s
@@ -9,3 +9,14 @@ main:  # This is the beginning of my main program.
    la $a0, userInput # This reads the user's string input.
    li $a1, 1000	# This assigns a space of 1000 bytes for the user.
    syscall 
+# This will remove the spaces in the beginning of the string.
+initial_space_deletion:
+
+   li $t8, 32 	# Initializes the space characeter.
+
+   lb $t7, 0($a0) 
+
+   beq $t8, $t7, initial_char_deletion #Removes initial space characters.
+
+   move $t7, $a0  # Stores characters in t7 register if they are not spaces.
+   j delete_after_spaces #Jumps to conditional.
