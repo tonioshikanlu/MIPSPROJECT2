038677588a3a8bd1838a8fcf7eeb8ade492a47ab
Oluwatoni Oshikanlu <39501338+tonioshikanlu@users.noreply.github.com>
2018-11-16 19:44:17 -0500

    This is the data declaration section. I've declared the label names that'll store my error messages and user input.

--- project2.s
+++ project2.s
@@ -1,1 +1,6 @@
-
+.data 
+   userInput:   .space 1000
+   empty_string_error:   .asciiz "Input is empty."
+   too_long:   .asciiz  "Input is too long."
+   invalidSpaces:   .asciiz   "Invalid base-35 number."
+.text 
