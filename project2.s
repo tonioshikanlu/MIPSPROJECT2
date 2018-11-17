1e097048ac2df1034515d6664161d6286820be6c
Oluwatoni Oshikanlu <39501338+tonioshikanlu@users.noreply.github.com>
2018-11-16 19:45:37 -0500

    This is my beginning to my main program. It holds the initial initializations. This loads the instruction to accept user input.

--- project2.s
+++ project2.s
@@ -4,3 +4,8 @@
    too_long:   .asciiz  "Input is too long."
    invalidSpaces:   .asciiz   "Invalid base-35 number."
 .text 
+main:  # This is the beginning of my main program.
+   li $v0, 8  
+   la $a0, userInput # This reads the user's string input.
+   li $a1, 1000	# This assigns a space of 1000 bytes for the user.
+   syscall 
